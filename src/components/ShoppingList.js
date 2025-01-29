be84e5bdb3f7a1c17a96af74396bc6e8ab321132
Joseph Andrew Phillips <josephandrewphillips@gmail.com>
2025-01-28 23:33:56 -0500

    Meets all deliverables, passes all tests

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+  const [cat, setCat] = useState('All')
+  const [col, setCol] = useState(items)
+
+  const newCat = col.filter(item => {
+    if (cat === 'All') {
+      return true
+    } else {
+      return item.category === cat
+    }
+  })
+
+  function handleChange(e) {
+    setCat(e.target.value)
+  }
+  
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +29,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {newCat.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
